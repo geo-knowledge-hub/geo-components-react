8aa6aa5107b5798cf426d70cf6bca80397c5adc1
Felipe Menino <efelipecarlos@gmail.com>
2022-07-16 11:09:54 -0300

    form: mocking apis used in the AdvancedSearchBar stories

--- src/lib/components/form/thematic/search/AdvancedSearchBar.stories.js
+++ src/lib/components/form/thematic/search/AdvancedSearchBar.stories.js
@@ -8,14 +8,78 @@
 
 import React from 'react';
 
+import withMock from 'storybook-addon-mock';
+
 import { Global } from '@emotion/react';
 
 import { AdvancedSearchBar as AdvancedSearchBarComponent } from './AdvancedSearchBar';
 
+import subjectsApiData from '@tests/mock/vocabularies/subjects-api.json';
+import resourceTypesApiData from '@tests/mock/vocabularies/resourcetypes-api.json';
+import namesApiData from '@tests/mock/vocabularies/names-api.json';
+import programmeActivityApiData from '@tests/mock/vocabularies/programmeactivities-api.json';
+import targetAudiencesApiData from '@tests/mock/vocabularies/targetaudiencestypes-api.json';
+import engagementPrioritiesApiData from '@tests/mock/vocabularies/engagementprioritiestypes-api.json';
+
 export default {
   title: 'Form/Search/Advanced search bar',
   component: AdvancedSearchBarComponent,
+  decorators: [withMock],
 };
+
+/**
+ * Mock API
+ */
+const mockApiConfig = [
+  {
+    url: '/api/vocabularies/subjects?size=&suggest=',
+    method: 'GET',
+    status: 200,
+    response: (request) => {
+      return subjectsApiData;
+    },
+  },
+  {
+    url: '/api/vocabularies/resourcetypes?size=&suggest=',
+    method: 'GET',
+    status: 200,
+    response: (request) => {
+      return resourceTypesApiData;
+    },
+  },
+  {
+    url: '/api/names?size=&suggest=',
+    method: 'GET',
+    status: 200,
+    response: (request) => {
+      return namesApiData;
+    },
+  },
+  {
+    url: '/api/vocabularies/geowptypes?size=&suggest=',
+    method: 'GET',
+    status: 200,
+    response: (request) => {
+      return programmeActivityApiData;
+    },
+  },
+  {
+    url: '/api/vocabularies/targetaudiencestypes?size=&suggest=',
+    method: 'GET',
+    status: 200,
+    response: (request) => {
+      return targetAudiencesApiData;
+    },
+  },
+  {
+    url: '/api/vocabularies/engagementprioritiestypes?size=&suggest=',
+    method: 'GET',
+    status: 200,
+    response: (request) => {
+      return engagementPrioritiesApiData;
+    },
+  },
+];
 
 /**
  * Component template
@@ -41,4 +105,11 @@ const Template = (args) => (
 export const Basic = Template.bind({});
 Basic.args = {
   placeholder: 'Search for something',
+  onSearch: (queryObject) => {
+    console.log(queryObject);
+  },
+};
+
+Basic.parameters = {
+  mockData: mockApiConfig,
 };
